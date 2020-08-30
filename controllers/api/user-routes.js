2dbcc7c22f198c36e6198b92b0da726a21ff02ed
Devmadia <agathaphan@icloud.com>
2020-08-29 22:31:08 -0500

    login and logout functionability added, session cookies created and tested; users can sign up and create an account to login with afterwards

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -41,11 +41,62 @@ router.post('/', (req, res) => {
         email: req.body.email,
         password: req.body.password
     })
-        .then(dbUserData => res.json(dbUserData))
-        .catch(err => {
-            console.log(err);
-            res.status(500).json(err);
+    //         .then(dbUserData => res.json(dbUserData))
+    //         .catch(err => {
+    //             console.log(err);
+    //             res.status(500).json(err);
+    //         });
+    // });
+    .then(dbUserData => {
+        req.session.save(() => {
+        req.session.user_id = dbUserData.id;
+        req.session.username = dbUserData.username;
+        req.session.loggedIn = true;
+    
+        res.json(dbUserData);
         });
+    })
+});
+
+// POST User session variable for logging in
+router.post('/login', (req, res) => {
+    User.findOne({
+      where: {
+        email: req.body.email
+      }
+    }).then(dbUserData => {
+        if (!dbUserData) {
+            res.status(400).json({ message: 'No user with that email address!' });
+            return;
+        }
+
+        const validPassword = dbUserData.checkPassword(req.body.password);
+
+        if (!validPassword) {
+            res.status(400).json({ message: 'Incorrect password!' });
+            return;
+        }
+
+        req.session.save(() => {
+            // declare session variables
+            req.session.user_id = dbUserData.id;
+            req.session.username = dbUserData.username;
+            req.session.loggedIn = true;
+
+            res.json({ user: dbUserData, message: 'You are now logged in!' });
+        });
+    });
+});
+// session variable to logout
+router.post('/logout', (req, res) => {
+    if (req.session.loggedIn) {
+        req.session.destroy(() => {
+          res.status(204).end();
+        });
+    }
+    else {
+        res.status(404).end();
+    }
 });
 
 // PUT /api/users/1
--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -100,7 +100,7 @@ router.get('/post/:id', (req, res) => {
             // pass data to template
             res.render('single-post', {
               post,
-              loggedIn: req.session.loggedIn
+              // loggedIn: req.session.loggedIn
             });
         })
         .catch(err => {
