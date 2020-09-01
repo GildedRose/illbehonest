cad7d95a44aaf1bdf96029a4e276c3457486c166
Anne <anne.k.plummer@gmail.com>
2020-08-31 22:11:03 -0500

    update to comment input

--- public/js/app.js
+++ public/js/app.js
@@ -10,4 +10,18 @@ $('.card-profile-stats-more-link').click(function(e){
     $(this).next('.card-profile-stats-more-content').slideToggle();
   });
   
-  
+  // Collapsible
+  var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
