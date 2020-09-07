191d0a96457b42589a261174f20714c0fed2f519
Eric Yarbrough <eric.yarbrough@utexas.edu>
2020-09-07 13:29:35 -0500

    fix add-post, edit homepage.handlebars, re-add post_summary to all routes

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -11,10 +11,9 @@ router.get('/', (req, res) => {
     attributes: [
       'id',
       'post_url',
-      'post_summary',
       'title',
+      'post_summary',
       'created_at',
-  
       [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
     ],
     order: [['created_at', 'DESC']],
@@ -53,13 +52,13 @@ router.get('/:id', (req, res) => {
     where: {
       id: req.params.id
     },
-    attributes: ['id', 'post_url', 'title', 'created_at',
-      [
-        sequelize.literal(
-          '(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'
-        ),
-        'vote_count'
-      ]
+    attributes: [
+      'id',
+      'post_url',
+      'title',
+      'post_summary',
+      'created_at',
+      [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
     ],
     include: [
       {
@@ -91,14 +90,14 @@ router.get('/:id', (req, res) => {
 
 // Create a Post
 router.post('/', withAuth, (req, res) => {
-// router.post('/', (req, res) => {
+  // router.post('/', (req, res) => {
   // expects {title: 'Taskmaster goes public!', post_url: 'https://taskmaster.com/press', user_id: 1}
   Post.create({
     title: req.body.title,
     post_url: req.body.post_url,
     user_id: req.session.user_id,
     category_id: req.body.category_id,
-    content: req.body.content
+    post_summary: req.body.post_summary
   })
     .then(dbPostData => res.json(dbPostData))
     .catch(err => {
@@ -109,7 +108,7 @@ router.post('/', withAuth, (req, res) => {
 
 // PUT /api/posts/upvote
 router.put('/upvote', withAuth, (req, res) => {
-// router.put('/upvote', (req, res) => {
+  // router.put('/upvote', (req, res) => {
   // custom static method created in models/Post.js
   Post.upvote({ ...req.body, user_id: req.session.user_id }, { Vote, Comment, User })
     .then(updatedVoteData => res.json(updatedVoteData))
@@ -132,7 +131,7 @@ router.put('/upvote', withAuth, (req, res) => {
 
 // Update a post's title
 router.put('/:id', withAuth, (req, res) => {
-// router.put('/:id', (req, res) => {
+  // router.put('/:id', (req, res) => {
   Post.update(
     {
       title: req.body.title
@@ -158,7 +157,7 @@ router.put('/:id', withAuth, (req, res) => {
 
 // Delete a Post
 router.delete('/:id', withAuth, (req, res) => {
-// router.delete('/:id', (req, res) => {
+  // router.delete('/:id', (req, res) => {
   Post.destroy({
     where: {
       id: req.params.id
